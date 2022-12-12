8531de4ee2e781d3e4e2aa4ebe2cebabc49e044c
rayan <rayanraihani9@gmail.com>
2022-12-12 16:33:08 +0100

    ajout du array pour ajouter des films

--- Cinema/Genre.php
+++ Cinema/Genre.php
@@ -1,22 +1,29 @@
 <?php
 class Genre{
     private string $_genre;
+    private array $_films;
 
     public function __construct($genre){
         $this->_genre = $genre;
+        $this->_films = [];
+            
 
     }
-    public function getGenre()
+    public function getGenre():string
     {
         return $this->_genre;
     }
 
     
-    public function seGenre($_genre)
+    public function setGenre($genre)
     {
         $this->_genre = $genre;
 
         return $this;
+    }
+    public function addFilm($film)
+    {
+        $this->_films[] = $film;
     }
 }
 
