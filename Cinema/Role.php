b620c524e70eeef761abd0f52cd44def151b80dc
rayan <rayanraihani9@gmail.com>
2022-12-12 16:33:59 +0100

    ajout du get set pour film et nom

--- Cinema/Role.php
+++ Cinema/Role.php
@@ -7,5 +7,17 @@ class Role{
         $this->_nom = $nom;
         $this->_films = $films;
     }
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+    public function getFilms()
+    {
+        return $this->_films;
+    }
+    public function setFilms($films)
+    {
+        $this->_films = $films;
+    }
 }
 ?>
