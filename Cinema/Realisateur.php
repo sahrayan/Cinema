592749ba27ca6d8639e278f8d37ee5c7c297e920
rayan <rayanraihani9@gmail.com>
2022-12-12 16:33:42 +0100

    ajout du array pour les films

--- Cinema/Realisateur.php
+++ Cinema/Realisateur.php
@@ -1,9 +1,13 @@
 <?php 
-class Directeur extends Personnage{
-    private $_films;
+class Realisateur extends Personnage{
+    private array $_films;
 
-    public function __construct(string $nom, string $prenom ){
-        $this->_films = $film;
+    public function __construct(string $nom, string $prenom, string $sex, string $anniversiare){
+        $this->_films =[];
+    }
+    public function addFilm($film)
+    {
+        $this->_films[] = $film;
     }
 }
 
