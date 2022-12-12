da3a74348fc99d1e08b4e80c64f9f172ec5bcec4
rayan <rayanraihani9@gmail.com>
2022-12-12 16:33:31 +0100

    ajouter des fichier require

--- Cinema/index.php
+++ Cinema/index.php
@@ -1,9 +1,20 @@
 <?php
-spl_autoload_register(function ($class_name) {
 
-require_once $class_name . '.php';
 
-});
+require_once 'Acteur.php';
+
+require_once 'Genre.php';
+
+require_once 'film.php';
+
+require_once 'Realisateur.php';
+
+require_once 'Personnage.php';
+
+require_once 'role.php';
+
+require_once 'Casting.php';
+
 
 
 
