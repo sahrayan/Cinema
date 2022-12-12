5d6f0007cf5156d2da5b6c701ca83c4c692d679a
Rayan R <rayanraihani9@gmail.com>
2022-12-12 11:35:10 +0100

    classe acteur vers personnage

--- Cinema/Acteur.php
+++ Cinema/Acteur.php
@@ -1,41 +1,22 @@
 <?php
-class Acteur{
-    private string $_nom;
-    private string $_prenom;
-    private string $_sex;
-    private string $_anniversaire;
+class Acteur extends Personnage {
+    private $_casting =[];
+        
+    public function __construct(string $nom,string $prenom,string $sex,$anniversaire){
+        parent::__construct($nom,$prenom,$sex,$anniversaire);
+    }
+            
 
-    public  function __construct(string $nom, string $prenom, string $sex, string $anniversaire){
-        $this->_nom = $nom;
-        $this->_prenom = $prenom;
-        $this->_sex = $sex;
-        $this->_anniversaire = $anniversaire;
-    }
-
-    public function getNom(){
-        return $this->_nom;
-    }
-    public function getPrenom(){
-        return $this->_prenom;
-    }
-    public function getSex(){
-        return $this->_sex;
-    }
-    public function getAnniversaire(){
-        return $this->_anniversaire;
+    public function addCasting($nouveauCasting){
+        array_push($this->_casting,$nouveauCasting);
     }
 
-    public function setNom(string $nom){
-        $this->_nom = $nom;
-    }
-    public function setPrenom(string $prenom){
-        $this->_prenom = $prenom;
-    }
-    public function setSex(string $sex){
-        $this->_sex = $sex;
-    }
-    public function setAnniversaire(string $anniversaire){
-        $this->_anniversaire = $anniversaire;
+
+    public function listCasting(){
+        echo "L'acteur " .$this->getNom()." ".$this->getPrenom(). "a participer au films:<br>";
+        foreach($this->_casting as $casting){
+            echo $casting->getFilm()->getTitle()." dans lequel il incarne " .$casting->getRole()->getName()."<br>";
+        }
     }
 }
 ?>
