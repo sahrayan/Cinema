0af722d34344c77449d718d8515cb2ea451286e5
rayan <rayanraihani9@gmail.com>
2022-12-12 16:33:18 +0100

    ajout des get set

--- Cinema/film.php
+++ Cinema/film.php
@@ -4,14 +4,42 @@ class film{
     private string $_date;
     private Genre $_genre;
     private Realisateur $_realisateur;
-    private int $_dure;
+    
 
-    public function __construct($titre,$date,$genre,$realisateur,$dure){
+    public function __construct($titre,$date,$genre,$realisateur){
         $this->_titre = $titre;
         $this->_date = $date;
         $this->_genre = $genre;
+        $this->_realisateur = $realisateur;
+        $this->_realisateur->addFilm($this);
+        $this->_genre->addFilm($this);
 
     }
+    public function getTitre(){
+        return $this->_titre;
+    }
+    public function getDate(){
+        return $this->_date;
+    }
+    public function getGenre(){
+        return $this->_genre;
+    }
+    public function getRealisateur(){
+        return $this->_realisateur;
+    }
+    public function setTitre($titre){
+        $this->_titre = $titre;
+    }
+    public function setDate($date){
+        $this->_date = $date;
+    }
+    public function setGenre(Genre $genre){
+        $this->_genre = $genre;
+    }
+    public function setRealisateur( Realisateur $realisateur){
+        $this->_realisateur = $realisateur;
+    }
+
 }
 
 
