b7e94380d0aacdc8191179e9222bc198a455ef4c
rayan <rayanraihani9@gmail.com>
2022-12-12 14:36:38 +0100

    rajout de prenom

--- Cinema/Realisateur.php
+++ Cinema/Realisateur.php
@@ -2,7 +2,7 @@
 class Directeur extends Personnage{
     private $_films;
 
-    public function __construct(string $nom, string ){
+    public function __construct(string $nom, string $prenom ){
         $this->_films = $film;
     }
 }
