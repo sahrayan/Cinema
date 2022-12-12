1844e602d95cde6e4366771fb6578406715f0f81
rayan <rayanraihani9@gmail.com>
2022-12-12 16:59:26 +0100

    echo

--- Cinema/index.php
+++ Cinema/index.php
@@ -16,7 +16,11 @@ require_once 'role.php';
 require_once 'Casting.php';
 
 
+$real1 = new Realisateur("Raihani","Rayan","Male","27-01-2004");
+$action = new Genre("Action");
+$film1 = new Film("Spider-Man","2002",$action,$real1);
 
+echo $film1;
 
 
 
