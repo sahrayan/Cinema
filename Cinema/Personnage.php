77bf51114af2affa6f83c0cd82e999a3a358f5e9
Rayan R <rayanraihani9@gmail.com>
2022-12-12 11:35:21 +0100

    modif pris de acteur a ici

---
+++ Cinema/Personnage.php
@@ -0,0 +1,41 @@
+<?php
+class Personnage{
+    private string $_nom;
+    private string $_prenom;
+    private string $_sex;
+    private int $_anniversaire;
+
+    public  function __construct(string $nom, string $prenom, string $sex,int $anniversaire){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_sex = $sex;
+        $this->_anniversaire = $anniversaire;
+    }
+
+    public function getNom(){
+        return $this->_nom;
+    }
+    public function getPrenom(){
+        return $this->_prenom;
+    }
+    public function getSex(){
+        return $this->_sex;
+    }
+    public function getAnniversaire(){
+        return $this->_anniversaire;
+    }
+
+    public function setNom(string $nom){
+        $this->_nom = $nom;
+    }
+    public function setPrenom(string $prenom){
+        $this->_prenom = $prenom;
+    }
+    public function setSex(string $sex){
+        $this->_sex = $sex;
+    }
+    public function setAnniversaire(int $anniversaire){
+        $this->_anniversaire = $anniversaire;
+    }
+}
+?>
