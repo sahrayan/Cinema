97eeb48bf37bd9e2e05c2fe839c3dd4fa710bead
rayan <rayanraihani9@gmail.com>
2022-12-13 16:48:31 +0100

    rajoue des role casting et affichage reussi de casting et inof film

--- Cinema/index.php
+++ Cinema/index.php
@@ -8,10 +8,19 @@ spl_autoload_register(function ($class_name) {
 });
 
 $real1 = new Realisateur("Raihani","Rayan","Male","27-01-2004");
+$acteur1 =new Acteur("Maguire","Tobey","Male","27/06/1975");
 $action = new Genre("Action");
+$role1 = new Role("Spider-Man");
 $film1 = new Film("Spider-Man","2002",$action,$real1);
+$casting1 = new Casting($film1,$role1,$acteur1);
 
-echo $film1;
+
+
+
+
+
+// echo $film1->getInfoFilm(). "<br>";
+echo $casting1->getCasting();
 
 
 
