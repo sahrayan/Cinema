32f457853688d0144515dc9577887b218e21cfb6
rayan <rayanraihani9@gmail.com>
2022-12-13 10:10:43 +0100

    function disprealisation pour afficher le realisateur du film

--- Cinema/Realisateur.php
+++ Cinema/Realisateur.php
@@ -3,12 +3,23 @@ class Realisateur extends Personnage{
     private array $_films;
 
     public function __construct(string $nom, string $prenom, string $sex, string $anniversiare){
+        parent::__construct($nom,$prenom,$sex,$anniversiare);
         $this->_films =[];
     }
     public function addFilm($film)
     {
         $this->_films[] = $film;
     }
+    public function dispRealisateur(){
+        $result = "film de $this";
+        foreach ($this->_films as $film){
+            $result.= $film;
+        }
+     return $result;
+    }
+
+
+
 }
 
 
