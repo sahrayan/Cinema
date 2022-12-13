13bf9c6f51912b2410f3adea51eb02678bd41d57
rayan <rayanraihani9@gmail.com>
2022-12-13 14:55:52 +0100

    ajout du array pour ajouter un role et un acteur au film

--- Cinema/Acteur.php
+++ Cinema/Acteur.php
@@ -1,21 +1,22 @@
 <?php
 class Acteur extends Personnage {
-    private $_casting =[];
+    private array $_casting;
         
     public function __construct(string $nom,string $prenom,string $sex,$anniversaire){
         parent::__construct($nom,$prenom,$sex,$anniversaire);
+        $this->_casting = [];
     }
             
 
     public function addCasting($nouveauCasting){
-        array_push($this->_casting,$nouveauCasting);
+        $this->_casting[] = $nouveauCasting;
     }
 
 
     public function listCasting(){
         echo "L'acteur " .$this->getNom()." ".$this->getPrenom(). "a participer au films:<br>";
-        foreach($this->_casting as $casting){
-            echo $casting->getFilm()->getTitle()." dans lequel il incarne " .$casting->getRole()->getName()."<br>";
+        foreach($this->_casting as $nouveauCasting){
+            echo $nouveauCasting->getFilm()->getTitle()." dans lequel il incarne " .$nouveauCasting->getRole()->getName()."<br>";
         }
     }
     
