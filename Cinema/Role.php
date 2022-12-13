003b01a65de810bb4adb98e75d8565b6546d3787
rayan <rayanraihani9@gmail.com>
2022-12-13 14:57:06 +0100

    ajour du addcasting et du tableau et du setnom qui ny etais pas

--- Cinema/Role.php
+++ Cinema/Role.php
@@ -2,19 +2,32 @@
 class Role{
     public  $_nom;
     private  $_films;
+    private array $_casting;
 
     public function __construct($nom,$films){
         $this->_nom = $nom;
         $this->_films = $films;
+        $this->_casting = [];
     }
+
+    public function addCasting($nouveauCasting){
+        $this->_casting[] = $nouveauCasting;
+    }
+
     public function getNom()
     {
         return $this->_nom;
     }
+    
+    public function setNom($nom){
+        $this->_nom = $nom;
+    }
+
     public function getFilms()
     {
         return $this->_films;
     }
+
     public function setFilms($films)
     {
         $this->_films = $films;
