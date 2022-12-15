11a475563a2a9cfa77dff402608e3f82c2cf50f0
Rayan <rayanraihani9@gmail.com>
2022-12-15 09:15:55 +0100

    nouveau film

--- Cinema/index.php
+++ Cinema/index.php
@@ -14,15 +14,20 @@ $role1 = new Role("Spider-Man");
 $film1 = new Film("Spider-Man","2002",$action,$real1);
 $casting1 = new Casting($film1,$role1,$acteur1);
 
+$real2 = new Realisateur("Raimi","Sam","Male","23/10/1959");
+$acteur2 = new Acteur("Franco","James","Male","20/08/1983");
+$action2 = new Genre("Action");
+$role2 = new Role("Green");
+$film2 = new Film("Green","2002",$action2,$real2);
+$casting2 = new Casting($film2,$role2,$acteur2);
 
 
 
 
-
-// echo $film1->getInfoFilm(). "<br>";
-echo $casting1->getCasting();
-echo $real1->dispRealisateur();
-echo $action->dispGenre();
+echo $film2->getInfoFilm(). "<br>";
+echo $casting2->getCasting();
+echo $real2->dispRealisateur();
+echo $action2->dispGenre();
 
 
 
